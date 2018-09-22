15847f60cb0e9f7010d433c1f7b2ee62aabeea1a
Benjamin Lee <benjamindlee@me.com>
2018-09-22 19:16:23 -0400

    Added thorough docs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 2
+
+For this lab, we used a special route for comment submission. To support the
+fact that comments needed to be submitted async, we immediately add the comment
+to the list while the submission is being sent to the route. Upon reloading of
+the page, the comment is shown via rendering using EJS.
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 const API_KEY = '7ad657f0-b77e-11e8-bf0e-e9322ccde4db';
 
-let comments = {};
+let comments = {}; // holds a mapping from object ids to lists of their comments
 
 // behavior for the index route
 app.get('/', (req, res) => {
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/gallery/:gallery_id', function(req, res) {
+    // show the list of objects for a given gallery
     fetch(`https://api.harvardartmuseums.org/object?apikey=${API_KEY}&gallery=${req.params.gallery_id}&size=999`)
     .then(response => response.json())
     .then(data => {
@@ -32,6 +33,7 @@ app.get('/gallery/:gallery_id', function(req, res) {
 });
 
 app.get('/object/:object_id', function(req, res) {
+  // shows the details for a given object id
   fetch(`https://api.harvardartmuseums.org/object/${req.params.object_id}?apikey=${API_KEY}`)
   .then(response => response.json())
   .then(data => {
@@ -41,12 +43,12 @@ app.get('/object/:object_id', function(req, res) {
 });
 
 app.get("/submit_comment/:object_id/:comment", function(req, res) {
+    // route for comment submission
     if (comments[req.params.object_id]) {
         comments[req.params.object_id].push(req.params.comment);
     } else {
         comments[req.params.object_id] = [req.params.comment]
     }
-    console.log(comments);
 });
 
 app.listen(port, host, () => {
